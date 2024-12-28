0071e51ed06206fd0d689df51567972b3b56a243
Harsh Sehgal <sehgalharsh700@gmail.com>
2024-12-28 22:25:18 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -12,11 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Use CORS middleware to allow requests from the frontend domain
-app.use(cors({
-  origin: 'https://health-ml-website.vercel.app', // Allow only your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  credentials: true, // Allow credentials (cookies or authorization headers)
-}));
+app.use(cors());
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
